b921c5d0dc485504411831316a9c9a8b5f37eea3
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-07-23 19:17:08 +0100

    add cube face

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-'use strict';
+//  import "./style.css";
 
-function counter() {
-  let seconds = 0;
-  setInterval(() => {
-    seconds += 1;
-    document.getElementById('app').innerHTML = `<p>You have been here for ${seconds} seconds.</p>`;
-  }, 1000);
-}
 
-counter();
+
